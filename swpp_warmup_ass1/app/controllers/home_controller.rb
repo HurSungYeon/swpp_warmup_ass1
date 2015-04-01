4bdf76ea0c0d87c57e93dc9cda32dd6567cef751
HurSungYeon <ethan0311@naver.com>
2015-04-01 20:22:43 +0900

    json response test

--- swpp_warmup_ass1/app/controllers/home_controller.rb
+++ swpp_warmup_ass1/app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def trylogin
+	@username = params[:identity]
+	@password = params[:password]
+	render :json => { "error": -1, "username": @username }
+  end
 end
