56a5b346c6ca2137795b4ee00944adb740350f0e
HurSungYeon <ethan0311@naver.com>
2015-04-01 20:40:33 +0900

    character error chk

--- swpp_warmup_ass1/app/controllers/home_controller.rb
+++ swpp_warmup_ass1/app/controllers/home_controller.rb
@@ -5,6 +5,17 @@ class HomeController < ApplicationController
   def trylogin
 	@username = params[:identity]
 	@password = params[:password]
-	render :json => { "error": -1, "username": @username }
+	if(@username.length<5 or @username.length>20)
+		@errorparam = -1
+		#render :json => { "error": -1, "username": @username }
+	elsif(@password.length<8 or @password.length>20)
+		@errorparam = -2
+		#render :json => { "error": -2, "username": @username }
+	else
+		@errorparam = 0
+		#render :json => { "error": 5, "username": @username }
+	end
+
+	render :json => { "error": @errorparam, "username": @username }
   end
 end
