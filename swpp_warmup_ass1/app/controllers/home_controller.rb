65f89168f5f660b958090b16319f6bfa2ede0b01
HurSungYeon <ethan0311@naver.com>
2015-04-01 21:31:21 +0900

    add Login Function

--- swpp_warmup_ass1/app/controllers/home_controller.rb
+++ swpp_warmup_ass1/app/controllers/home_controller.rb
@@ -5,17 +5,24 @@ class HomeController < ApplicationController
   def login
 	@username = params[:username]
 	@password = params[:password]
-	if(@username.length<5 or @username.length>20)
-		@errorparam = -1
-		#render :json => { "error": -1, "username": @username }
-	elsif(@password.length<8 or @password.length>20)
-		@errorparam = -2
-		#render :json => { "error": -2, "username": @username }
-	else
-		@errorparam = 0
-		#render :json => { "error": 5, "username": @username }
-	end
+
+	@errorparam = -1
 
 	render :json => { "error": @errorparam, "username": @username }
+  end
+
+  def signup
+	@username = params[:username]
+        @password = params[:password]
+        if(@username.length<5 or @username.length>20)
+                @errorparam = -1
+        elsif(@password.length<8 or @password.length>20)
+                @errorparam = -2
+        else
+                @errorparam = 0
+        end
+
+        render :json => { "error": @errorparam, "username": @username }
+
   end
 end
